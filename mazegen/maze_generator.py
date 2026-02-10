04729b9119879fb59ceb8cfa8284804e5e353953
maya-fakih <alfakihmaya1@gmail.com>
2026-02-11 01:32:26 +0200

    common functions for all generators
    in this commit i aded the functions has_wall that checks if a cell i will be visiting in all my algorithms has a wall in a direction specified in the parameters, it also takes into consideration the outer border and the cells that are in the self.logo_cells as boundries as well so that if i wantto open a wall in any adjacent cell to these i cannot open this common wall.... the remove wall uses bit wise and this is pretty simple to understand try to trace it on paper and see we do the current cell and the complement of the wall print so if i have a cell 1111 and i want to open north 0001 i do (1111 & 1110) in this way only the north cell will become & 0 thus = 0 and opens that wall... to simplify the codes i added them as kindof a gloabal variable in the base class maze generator... last but certainly not least i added the initialization of the 42 logo took forever to reverse enginner then i decided to just hard code the shape it is very hard... IMPORTANT NOTE FOR AGNES: if you notice i added a self.path this in all the algorithms will have the visited cells each in order okay? the tuple is the x, y coordinates, the int is the value in this cell from 0 to 15 and the bool tells you if this cell is on the path to the solution or not... what i will do tonight is the prims logic you will understand more why these will be useful in the next commit... for now just know that this is our starting point in animating the maze generation with the path to solve it then we will have to refresh the user interface and have just the regular maze from the .txt file that will be used to animate the solution so we will use this self.path only to animate the maze generation if that was all that we are doing okay we are only generating the maze we will have to decide on the user inputs for each state but that is for later... hopefully i will have the prim ready before my classes today

--- mazegen/maze_generator.py
+++ mazegen/maze_generator.py
@@ -1,8 +1,27 @@
-from typing import Any, List, Dict
+from typing import Any, List, Dict, Tuple
 from abc import ABC, abstractmethod
-
+import sys
 
 class MazeGenerator(ABC):
+    NORTH = 0b0001
+    EAST = 0b0010
+    SOUTH = 0b0100
+    WEST = 0b1000
+
+    MASK = {
+        'N': NORTH,
+        'E': EAST,
+        'S': SOUTH,
+        'W': WEST
+    }
+
+    OPPOSITE = {
+        NORTH: SOUTH,
+        SOUTH: NORTH,
+        EAST: WEST,
+        WEST: EAST
+    }
+
     def __init__(self, settings_dict: dict):
         self.width = settings_dict.get("width")
         self.height = settings_dict.get("height")
@@ -15,14 +34,87 @@ class MazeGenerator(ABC):
         self.algorithm = settings_dict.get("algorithm", "dfs")
         self.shape = settings_dict.get("shape", "square")
         self.maze = List[List[int]]
+        self.path = [Tuple[Tuple[int, int]], int, bool]
+        self.logo_cells = set()
+        self._add_42_logo()
 
-    # add function to initialize all sides closed
-    # add function to draw the 42 logo (should print error message if it cant print 42 logo w continues normally)
+    
+    def has_wall(self, location: Tuple, direction: str) -> bool:
+        x, y = location
+        
+        if direction == 'W' and x == 0: return False
+        if direction == 'E' and x == self.width-1: return False
+        if direction == 'N' and y == 0: return False
+        if direction == 'S' and y == self.height-1: return False
+
+        if direction == 'N': nx, ny = x, y-1
+        elif direction == 'S': nx, ny = x, y+1
+        elif direction == 'E': nx, ny = x+1, y
+        elif direction == 'W': nx, ny = x-1, y
+        
+        if (x, y) in self.logo_cells or (nx, ny) in self.logo_cells:
+            return False
+
+        cell_value = self.maze[x][y]
+        mask = self.MASK[direction]
+        return (cell_value & mask) != 0
+
+
+    def remove_wall(self, cell: Tuple, direction: str) -> None:
+        x, y = cell
+
+        if direction == 'N': nx, ny = x, y-1
+        elif direction == 'S': nx, ny = x, y+1
+        elif direction == 'E': nx, ny = x+1, y
+        elif direction == 'W': nx, ny = x-1, y
+        
+        mask = self.MASK[direction]
+        opposite_mask = self.OPPOSITE[mask]
+        
+        self.maze[x][y] &= ~mask
+        self.maze[nx][ny] &= ~opposite_mask
+
+
+    def _add_42_logo(self):
+        """Create 42 logo pattern in the maze if dimensions allow it."""
+        if self.width < 10 or self.height < 10:
+            sys.stderr("Could not draw 42 logo. (dimentions too small)")
+            return
+        
+        start_x = self.width // 2 - 4
+        start_y = self.height // 2 - 2
+        
+        self.logo_cells.add((start_x, start_y))
+        self.logo_cells.add((start_x, start_y + 1))
+        self.logo_cells.add((start_x, start_y + 2))
+        
+        self.logo_cells.add((start_x + 1, start_y + 2))
+        
+        self.logo_cells.add((start_x + 2, start_y + 2))
+        self.logo_cells.add((start_x + 2, start_y + 3))
+        self.logo_cells.add((start_x + 2, start_y + 4))
+                
+        self.logo_cells.add((start_x + 4, start_y))
+        self.logo_cells.add((start_x + 4, start_y + 2))
+        self.logo_cells.add((start_x + 4, start_y + 3))
+        self.logo_cells.add((start_x + 4, start_y + 4))
+        
+        self.logo_cells.add((start_x + 5, start_y))
+        self.logo_cells.add((start_x + 5, start_y + 2))
+        self.logo_cells.add((start_x + 5, start_y + 4))
+
+        self.logo_cells.add((start_x + 6, start_y))
+        self.logo_cells.add((start_x + 6, start_y + 1))
+        self.logo_cells.add((start_x + 6, start_y + 2))
+        self.logo_cells.add((start_x + 6, start_y + 4))
+        
+        for x, y in self.logo_cells:
+            if 0 <= x < self.width and 0 <= y < self.height:
+                self.maze[x][y] = 15 
+
 
     @abstractmethod
     def generate(self) -> Any:
-        # the str is the order of the cells it creates in order
-        # so we can animate it later
         pass
 
     def write_to_file(self) -> None:
