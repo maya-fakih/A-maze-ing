c072c0d2e24c5258963906bb2a7bb22ee6802dc7
maya-fakih <alfakihmaya1@gmail.com>
2026-02-11 09:45:44 +0200

    more common helper functions
    i added the get neighbors also as a common function for all classes as well initializing the main maze since what we had before is only a type hint no real values so i noticed it wont make sense to call the init logo function o n a non existent maze... i also initialized generically all maze cells to 0s then each algorithm can have their own initialize maze function that sets the values as needed for that algorithm withought over writing the self.logo_cells as these have to be blocked cells just as we have the outer wall cells as umm guards you can say? same thing here... i will implement the rest of the alg now but i thought its better to push the helpers alone for clarity

--- mazegen/default_generator.py
+++ mazegen/default_generator.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Dict
+from typing import Any, List, Dict, Tuple
 from maze_generator import MazeGenerator
 from collections import deque
 import random
@@ -10,6 +10,16 @@ class BasicGenerator(MazeGenerator):
     def __init__(self, settings_dict):
         super().__init__(settings_dict)
 
+    def initialize_maze(self) -> None:
+        for x in range(self.width):
+            for y in range(self.height):
+                if (x, y) not in self.logo_cells:
+                    self.maze[x][y] = 15
+    
+
     def generate(self) -> Any:
-        pass
+        reached_goal = False
+        self.path.extend(self.logo_cells)
+        self.initialize_maze()
+        
 
--- mazegen/maze_generator.py
+++ mazegen/maze_generator.py
@@ -33,10 +33,20 @@ class MazeGenerator(ABC):
         self.flag_color = settings_dict.get("flag_color", "blue")
         self.algorithm = settings_dict.get("algorithm", "dfs")
         self.shape = settings_dict.get("shape", "square")
-        self.maze = List[List[int]]
-        self.path = [Tuple[Tuple[int, int]], int, bool]
+        self.maze = [[0 for _ in range(self.height)] for _ in range(self.width)]
+        self.path : List[Tuple[Tuple[int, int]], int, bool] = []
         self.logo_cells = set()
         self._add_42_logo()
+
+    
+    @abstractmethod
+    def generate(self) -> Any:
+        pass
+
+
+    @abstractmethod
+    def initialize_maze(self) -> None:
+        pass
 
     
     def has_wall(self, location: Tuple, direction: str) -> bool:
@@ -73,6 +83,22 @@ class MazeGenerator(ABC):
         
         self.maze[x][y] &= ~mask
         self.maze[nx][ny] &= ~opposite_mask
+
+
+    def get_neighbors(self, cell: Tuple) -> List[Tuple]:
+        x, y = cell
+        possible = [
+            (x+1, y, 'E'),
+            (x-1, y, 'W'), 
+            (x, y+1, 'S'),
+            (x, y-1, 'N')
+        ]
+        neighbors = []
+        for nx, ny, direction in possible:
+            if 0 <= nx < self.width and 0 <= ny < self.height:
+                if (nx, ny) not in self.logo_cells:
+                    neighbors.append((nx, ny, direction))
+        return neighbors
 
 
     def _add_42_logo(self):
@@ -112,10 +138,6 @@ class MazeGenerator(ABC):
             if 0 <= x < self.width and 0 <= y < self.height:
                 self.maze[x][y] = 15 
 
-
-    @abstractmethod
-    def generate(self) -> Any:
-        pass
 
     def write_to_file(self) -> None:
         pass
