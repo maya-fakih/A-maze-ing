d00c4027d9b6c15cb759083310c284e82cebf6c0
maya-fakih <alfakihmaya1@gmail.com>
2026-02-12 23:23:13 +0200

    all issues fixed

--- mazegen/testing.py
+++ mazegen/testing.py
@@ -1,19 +1,19 @@
 from default_generator import BasicGenerator
 
 
-
 config = {
     "width": 25,
     "height": 20,
     "entry": (0, 0),
     "exit": (18, 19),
     "output_file": "test.txt",
-    "perfect": True,
+    "perfect": False,
     "wall_color": "white",
     "flag_color": "blue",
     "algorithm": "dfs",
     "shape": "square"
 }
+
 
 def print_maze(self):
     """Print the maze grid as binary values"""
@@ -23,8 +23,8 @@ def print_maze(self):
             row.append(f"{self.maze[x][y]:04b}")
         print(" ".join(row))
 
+
 generator = BasicGenerator(config)
 generator.generate()
 generator.display_ascii()
 print(f" the generator is perfect flag is: {generator.perfect}")
-
