38968bf1d2cf3e26e96d636328756182912cff73
Agnes <164067781+abimoussagnes@users.noreply.github.com>
2026-02-13 18:56:34 +0200

    refacstored, added a factory method, started with the display

--- learning_shapes/circle_logic.py
+++ learning_shapes/circle_logic.py
@@ -4,7 +4,9 @@ height = 20
 
 for row in range(width):
     for col in range(height):
-        if (row - col) == (width // 2) or (row + col) == (width // 2) or (row + col) == (width // 2)**2:
-            print("*", end = "")
+        if ((row - col) == (width // 2) or
+                (row + col) == (width // 2) or
+                (row + col) == (width // 2)**2):
+            print("*", end="")
         else:
-            print("", end = "")
+            print("", end="")
--- mazegen/huntkill_generator.py
+++ mazegen/huntkill_generator.py
@@ -4,9 +4,9 @@ from .maze_generator import MazeGenerator
 class HuntKillGenerator(MazeGenerator):
     def __init__(self, settings_dict):
         super().__init__(settings_dict)
-        
+
     def generate(self):
         return super().generate()
-    
+
     def initialize_maze(self):
         return super().initialize_maze()
