52d22d3ba6df0e4df0bfb70224ea9e22ae9c7adf
Agnes <164067781+abimoussagnes@users.noreply.github.com>
2026-02-14 14:35:02 +0200

    finally added display in teminal

--- learning_shapes/diamond_logic.py
+++ learning_shapes/diamond_logic.py
@@ -9,23 +9,21 @@ for i in range(width):
     path = []
     if forward:
         j1 = width // 2 - i
-        j2 = width //2 + 1 + i
+        j2 = width // 2 + 1 + i
     else:
         j1 = width // 2 + j
-        j2 = width //2 - j
+        j2 = width // 2 - j
         j += 1
     if j1 < 0:
         forward = False
         j = 0
-
-    
 
     path.extend([(i, j1), (i, j2)])
 
 for i in range(width):
     for j in range(height):
         if (i, j) in path:
-            print("*", end = "")
+            print("*", end="")
         else:
-            print("", end = "")
+            print("", end="")
     print()
