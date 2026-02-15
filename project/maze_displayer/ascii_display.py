13b885bad44fc1cbc0ca86879e9ce2a3e172278d
maya-fakih <alfakihmaya1@gmail.com>
2026-02-15 23:22:29 +0200

    drawing ascci spaces
    made the maze print in ascii which was a fun experiment that worked but the drawing corners is hard.. we need to check if the c orners of a cell are neighbors to a logo cell to draw that + in the logo cell color i will try to fix it but i will need to add multiple if else ifs? or maybe get their neighbors i will try... thought i would commit this too keep at least a half working version i know where the issue is from

--- project/maze_displayer/ascii_display.py
+++ project/maze_displayer/ascii_display.py
@@ -29,8 +29,11 @@ color_map = {
 }
 
 
-def _print_corner(wall_code: str, reset_code: str):
-    print(wall_code + "█" + reset_code, end="")
+def _print_corner(wall_code: str, reset_code: str, logo_cells: set = None, flag_code: str = None):
+    if logo_cells in logo_cells:
+        print(flag_code + "█" + reset_code, end="")
+    else:
+        print(wall_code + "█" + reset_code, end="")
 
 
 def _print_horizontal_wall(
@@ -40,6 +43,8 @@ def _print_horizontal_wall(
     W: int,
     grid: list[list],
     wall_code: str,
+    flag_code: str,
+    logo_cells: set,
     reset_code: str,
     SOUTH: int,
 ):
@@ -49,7 +54,10 @@ def _print_horizontal_wall(
         cell_row = r // 2 - 1
         cell_col = c // 2
         if grid[cell_col][cell_row] & SOUTH:
-            print(wall_code + "███" + reset_code, end="")
+            if (cell_col, cell_row) in logo_cells:
+                print(flag_code + "███" + reset_code, end="")
+            else:
+                print(wall_code + "███" + reset_code, end="")
         else:
             print("   ", end="")
 
@@ -60,6 +68,8 @@ def _print_vertical_wall(
     W: int,
     grid: list[list],
     wall_code: str,
+    flag_code: str,
+    logo_cells: set,
     reset_code: str,
     EAST: int,
 ):
@@ -69,7 +79,10 @@ def _print_vertical_wall(
         cell_row = r // 2
         cell_col = c // 2 - 1
         if grid[cell_col][cell_row] & EAST:
-            print(wall_code + "█" + reset_code, end="")
+            if (cell_col, cell_row) in logo_cells:
+                print(flag_code + "█" + reset_code, end="")
+            else:
+                print(wall_code + "█" + reset_code, end="")
         else:
             print(" ", end="")
 
@@ -114,15 +127,15 @@ def display_gen(maze: MazeGenerator):
         for c in range(2 * W + 1):
             # Corners
             if r % 2 == 0 and c % 2 == 0:
-                _print_corner(wall_code, reset_code)
+                _print_corner(wall_code, reset_code, logo_cells, flag_code)
             # Horizontal walls
             elif r % 2 == 0 and c % 2 == 1:
                 _print_horizontal_wall(
-                    r, c, H, W, grid, wall_code, reset_code, SOUTH)
+                    r, c, H, W, grid, wall_code, flag_code, logo_cells, reset_code, SOUTH)
             # Vertical walls
             elif r % 2 == 1 and c % 2 == 0:
                 _print_vertical_wall(
-                    r, c, W, grid, wall_code, reset_code, EAST)
+                    r, c, W, grid, wall_code, flag_code, logo_cells, reset_code, EAST)
             # Cell interior
             else:
                 _print_cell_interior(r, c, logo_cells, flag_code, reset_code, entry, exit)
